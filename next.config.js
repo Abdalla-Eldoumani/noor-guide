db4f9577d8f351a0b6f79ba86c9a6442fdf7b8c4
Abdalla Eldoumani <aamsdoumani@gmail.com>
2026-04-30 16:31:52 -0600

    wrap next config with next intl plugin

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Remove X-Powered-By header for security
   poweredByHeader: false,
-  // Enable gzip compression
   compress: true,
-  // Strict React mode for catching bugs early
   reactStrictMode: true,
 };
+
+const createNextIntlPlugin = require("next-intl/plugin");
+const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
-module.exports = withBundleAnalyzer(nextConfig);
+module.exports = withBundleAnalyzer(withNextIntl(nextConfig));
