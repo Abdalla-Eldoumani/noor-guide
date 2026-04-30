fd134c3916f46cbea1e620baef7902623374f4de
Abdalla Eldoumani <aamsdoumani@gmail.com>
2026-04-30 16:45:40 -0600

    use locale aware link in learn dashboard

--- src/app/[locale]/learn/LearnDashboardClient.tsx
+++ src/app/[locale]/learn/LearnDashboardClient.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Link from "next/link";
+import { Link } from "@/i18n/navigation";
 import { useProgress } from "@/hooks/useProgress";
 import { LessonCard } from "@/components/learn/LessonCard";
 import { ProgressBar } from "@/components/ui/ProgressBar";
